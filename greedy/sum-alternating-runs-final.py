9714e0f76799868b07868c88b61ef06a42df04e7
Abhilashini A J <aj.abhilashini@ymail.com>
2025-06-11 18:21:19 +0530

    Create sum-alternating-runs-final.py
    Working solution with final, refined changes.

---
+++ greedy/sum-alternating-runs-final.py
@@ -0,0 +1,29 @@
+def getMaxSum(arr):
+  n = len(arr)
+  max_sum = 0
+  i = 0
+
+  while i < n-1:
+    curr_max = arr[i]
+
+    if arr[i] > arr[i+1]:
+      while i < n-1 and arr[i] > arr[i+1]:
+        i += 1
+        curr_max = max(curr_max, arr[i])
+      max_sum += curr_max
+    elif arr[i] < arr[i+1]:
+      while i < n-1 and arr[i] < arr[i+1]:
+        i += 1
+        curr_max = max(curr_max, arr[i])
+      max_sum += curr_max
+    else:
+      i += 1
+
+  return max_sum
+
+def test_getMaxSum():
+    assert getMaxSum([5, 4, 3, 2]) == 5
+    assert getMaxSum([1, 2, 3, 2, 1, 4, 5]) == 11
+    assert getMaxSum([1, 2, 1, 2, 1]) == 8
+    
+test_getMaxSum()
